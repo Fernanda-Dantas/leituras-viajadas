c43fc6a2cfe135e59ce6aafb15131a76371e7328
Fernanda Dantas <fer.dantas07@gmail.com>
2022-08-30 14:36:12 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,38 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/init-accordion.js';
+import initTabNav from './modules/tab-nav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/initTooltip.js';
+import initDropDownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initNumerosApi from './modules/fetch-numeros.js';
+
+
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropDownMenu();
+initMenuMobile();
+initFuncionamento();
+initNumerosApi();
+
+
+const contato = document.querySelector('#contato');
+const dados = {}
+
+
+function handleChange(event) {
+    console.log(dados)
+    dados[event.target.name] = event.target.value
+}
+
+contato.addEventListener('change', handleChange)
+console.log('teste');
+
+
